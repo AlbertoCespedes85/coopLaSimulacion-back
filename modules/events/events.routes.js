a4ccd6a5d980f11f3b76e8a6ed74499bea6d3268
Alberto <pendientintin@hotmail.com>
2025-12-04 09:22:26 +0100

    proteccion events

--- modules/events/events.routes.js
+++ modules/events/events.routes.js
@@ -10,20 +10,21 @@ import { handleMulterError } from '../../helpers/handleMulterError.js'
 import { editDataEventSchema } from '../../schemas/editDataEventSchema.js'
 import { editSectionEventSchema } from '../../schemas/editSectionEventSchema.js'
 import { editEventKeyPointSchema } from '../../schemas/editEventKeyPointSchema.js'
+import { verifyAdminFull } from '../../middlewares/verifyAdminFull.js'
 const router = express.Router()
 
 router.get('/events', eventsControllers.getEventData)
 router.get('/calendar/month/:year/:month', eventsControllers.getEventsByMonth)
 router.get('/calendar/day/:date', eventsControllers.getEventsByDay)
-router.post('/createEvent', verifyToken, uploadImageAny('events'), validateFormsEvent(createEventSchema), eventsControllers.createEvent)
+router.post('/createEvent', verifyToken, uploadImageAny('events'),verifyAdminFull, validateFormsEvent(createEventSchema), eventsControllers.createEvent)
 router.get('/event/:id', eventsControllers.getEventById)
 router.get('/editEvent/:id', verifyToken, eventsControllers.getEventById);
-router.put('/editData/:id', verifyToken, uploadImageSingle("events"), validateFormsEvent(editDataEventSchema), eventsControllers.editDataEvent);
-router.put('/editSection', verifyToken, validateFormsEvent(editSectionEventSchema), eventsControllers.editDataSection);
-router.put('/delkeypoint' , verifyToken , eventsControllers.delKeypoint);
-router.put('/addkeypoint/:id' , verifyToken, validateFormsEvent(editEventKeyPointSchema), eventsControllers.addKeypoint);
-router.delete('/delSectionImage', verifyToken, eventsControllers.deleteSectionImage);
-router.put('/addSectionImages', verifyToken, uploadImageMulti("events"), eventsControllers.addSectionImages);
-router.delete('/deleteSection/:id', verifyToken, eventsControllers.deleteSection);
+router.put('/editData/:id', verifyToken, uploadImageSingle("events"),verifyAdminFull, validateFormsEvent(editDataEventSchema), eventsControllers.editDataEvent);
+router.put('/editSection', verifyToken,verifyAdminFull, validateFormsEvent(editSectionEventSchema), eventsControllers.editDataSection);
+router.put('/delkeypoint' , verifyToken ,verifyAdminFull, eventsControllers.delKeypoint);
+router.put('/addkeypoint/:id' , verifyToken,verifyAdminFull, validateFormsEvent(editEventKeyPointSchema), eventsControllers.addKeypoint);
+router.delete('/delSectionImage', verifyToken,verifyAdminFull, eventsControllers.deleteSectionImage);
+router.put('/addSectionImages', verifyToken,verifyAdminFull, uploadImageMulti("events"), eventsControllers.addSectionImages);
+router.delete('/deleteSection/:id', verifyToken,verifyAdminFull, eventsControllers.deleteSection);
 
 export default router;
